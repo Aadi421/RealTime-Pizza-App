ad32a846a3284d617d440c15b692cec9129dc956
Adarsh Chaudhary <adarshchaudhary434@gmail.com>
2021-03-17 20:30:43 +0530

    initial

---
+++ server.js
@@ -0,0 +1,24 @@
+const express=require("express");
+const app=express();
+const path=require("path")
+const PORT=process.env.PORT || 8000;
+const ejs=require("ejs");
+const expressLayout=require("express-ejs-layouts");
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.render("home")
+})
+
+
+app.use(expressLayout);
+// set template engine
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'./resources/views'))
+// server fireup
+app.listen(PORT,()=>{
+    console.log(`Yup! My server listening on ${PORT}`);
+})
