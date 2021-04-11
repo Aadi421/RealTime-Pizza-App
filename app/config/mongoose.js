b8d1eeab69f00dcfd20ab32721a6665f5143fad0
Adarsh Chaudhary <adarshchaudhary434@gmail.com>
2021-04-11 12:10:36 +0530

    change in db connection

--- app/config/mongoose.js
+++ app/config/mongoose.js
@@ -9,7 +9,6 @@ mongoose.connect('mongodb+srv://Aadi421:Aadi@421@cluster0.eerxi.mongodb.net/myFi
     useUnifiedTopology: true
 });
 
-
 //acquire  the connection(check if is it successful)
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
