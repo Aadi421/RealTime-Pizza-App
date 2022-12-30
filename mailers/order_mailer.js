a97452e25244b50f56f1a99d19e572140736bcf3
Adarsh Chaudhary <adarshchaudhary434@gmail.com>
2022-12-30 13:31:35 +0530

    save

--- mailers/order_mailer.js
+++ mailers/order_mailer.js
@@ -8,17 +8,17 @@ module.exports.newOrder=(order) => {
    
    let htmlString=nodeMailer.renderTemplate({order:order,moment:moment},'/orders/newOrder.ejs');
   // send mail with defined transport object
- nodeMailer.transporter.sendMail({
-    from: 'adarshchaudhary434@gmail.com', // sender address
-    to:order.customerId.email, // list of receivers
-    subject: "Order palced!",// Subject line
-    // text: "Hello world?", // plain text body
-    html:htmlString, // html body
- },(err,info) =>
-    {
-      if(err){console.log(err,'error in sending mails'); return}
-      console.log('message sent',info);
-      return;
-    });
+  nodeMailer.transporter.sendMail({
+      from: 'adarshchaudhary434@gmail.com', // sender address
+      to:order.customerId.email, // list of receivers
+      subject: "Order palced!",// Subject line
+      // text: "Hello world?", // plain text body
+      html:htmlString, // html body
+  },(err,info) =>
+      {
+        if(err){console.log(err,'error in sending mails'); return}
+        console.log('message sent',info);
+        return;
+      });
 }
 
